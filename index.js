70f24f8d0507a53227df0bf863fdeabf27a337e0
Adam <ligmaotoko@gmail.com>
2025-01-27 15:20:46 +0100

    add a random num to generate a random color rgb

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
  const container = document.getElementById('container')
  const gridCell = document.createElement('div')
 
-function createGrid(num) {
-    for (let i = 0; i < num; i++) {
+ let gridNum;
+
+function createGrid(gridNum) {
+    container.innerHTML = ''
+    const cellSize = 700 / gridNum
+    function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      }
+    for (let i = 0; i < gridNum * gridNum; i++) {
         const gridCell = document.createElement('div');
         gridCell.classList.add('cell')
-        container.appendChild(gridCell)
 
+        gridCell.style.width = `${cellSize}px`
+        gridCell.style.height = `${cellSize}px`
         gridCell.addEventListener('mouseover', () => {
-            gridCell.style.backgroundColor = 'red';
+            const randomColor = `rgb(${randomIntFromInterval(0, 255)}, ${randomIntFromInterval(0, 255)},
+             ${randomIntFromInterval(0, 255)}) `;
+            gridCell.style.backgroundColor = randomColor
         });
         gridCell.addEventListener('mouseout', () => {
             gridCell.style.backgroundColor = '';
@@ -18,5 +28,15 @@ function createGrid(num) {
     }
 }
 
-createGrid(16 * 16)
+
+const btn = document.getElementById('button')
+btn.addEventListener('click', () => {
+    const gridNum =  prompt('How many grids do you want?')
+    if(!isNaN(gridNum) && gridNum <= 100 && gridNum > 0) {
+        alert(`You selected ${gridNum} grids!`)
+        createGrid(gridNum)
+    } else {
+        alert('Please enter a valid number greater than 0, and below 100')
+    }
+})
 
